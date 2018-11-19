4e7b0f129e534ce06ba123125e0a7605e979597b
Olivier JM <manolivier93@gmail.com>
2018-11-19 09:42:02 +0200

    added the basic native base components

---
+++ components/UpdateCard.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+
+export default class UpdateCard extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Card style={{flex: 0}}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri: 'Image URL'}} />
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>April 15, 2016</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Image source={{uri: 'Image URL'}} style={{height: 200, width: 200, flex: 1}}/>
+                <Text>
+                Projecting surrounded literature yet delightful alteration but bed men. Open are from 
+                long why cold. If must snug by upon sang loud left. 
+                As me do preference entreaties compliment motionless ye literature. 
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent textStyle={{color: '#87838B'}}>
+                  <Icon name="logo-github" />
+                  <Text>1,926 stars</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
+}
