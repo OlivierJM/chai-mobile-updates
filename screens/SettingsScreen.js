9e1e530fe85dee84c8616ab83bfff53a6963123d
Olivier JM <manolivier93@gmail.com>
2018-11-19 09:50:06 +0200

    cleaned the settings pane

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { Text } from 'react-native'
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -9,6 +9,10 @@ export default class SettingsScreen extends React.Component {
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <Text>
+        Settings could be here
+      </Text>
+    )
   }
 }
