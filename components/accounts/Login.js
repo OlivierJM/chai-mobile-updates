76895dd3e7e1dc49eb03217af89d0b37b0086cce
Olivier JM <manolivier93@gmail.com>
2018-11-21 11:38:01 +0200

    user registration is working

--- components/accounts/Login.js
+++ components/accounts/Login.js
@@ -6,51 +6,62 @@ import {
   Form,
   Item,
   Input,
-  Label, 
+  Label,
   View,
-  Button, 
-  Text, Body
-} from "native-base";
+  Button,
+  Text,
+  Body
+} from "native-base"
 
 export default class Login extends Component {
-    state = {
-        phone: '',
-        password: ''
-    }
+  state = {
+    phone: "",
+    password: ""
+  }
 
-    handleLogin = () => {
-        const { phone, password } = this.state
+  handleLogin = () => {
+    const { phone, password } = this.state
+    // Log the user in and route to Home
 
-        alert(phone, password) 
-
-    }
+    return this.props.navigation.navigate("Home")
+  };
   render() {
-      const { phone, password } = this.state
+    const { phone, password } = this.state;
     return (
       <Container>
         <Content>
           <Form>
             <Item floatingLabel>
               <Label>Phone Number</Label>
-              <Input keyboardType='phone-pad' value={phone} onChangeText={text => this.setState({phone: text})}/>
+              <Input
+                    keyboardType="phone-pad"
+                    value={phone}
+                    onChangeText={text => this.setState({ phone: text })}
+              />
             </Item>
             <Item floatingLabel last>
               <Label>Password</Label>
-              <Input value={password} onChangeText={text => this.setState({password: text})}/>
+              <Input
+                value={password}
+                onChangeText={text => this.setState({ password: text })}
+              />
             </Item>
           </Form>
-          <Body>
-              <Button transparent onPress={this.handleLogin}>
-                  <Text>Login</Text>
-              </Button>    
+          <Body style={{marginTop: 25}}>
+                <Button onPress={this.handleLogin}>
+                <Text>Login</Text>
+                </Button>
           </Body>
           <Body>
-              <Button transparent onPress={() => this.props.navigation.navigate('Register')}>
-                  <Text>Register</Text>
-              </Button>    
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("Register")}
+            >
+              <Text>Register</Text>
+            </Button>
           </Body>
         </Content>
       </Container>
-    );
+    )
   }
 }
--- components/accounts/Register.js
+++ components/accounts/Register.js
@@ -11,34 +11,80 @@ import {
   Button, 
   Text, Body
 } from "native-base";
+import { Accounts } from 'react-native-meteor'
 
 export default class Login extends Component {
+    state = {
+        name: '',
+        phone : '',
+        confirmPassword: '',
+        password: '',
+        error: ''
+    }
+
+    handleRegister = () => {
+        const { name, phone, confirmPassword, password } = this.state
+        const user = {
+            username: phone,
+            password,
+            profile: {
+                name
+            }
+        }
+        // register the user and take them to the home page
+        Accounts.createUser(user, err => {
+            if (err) {
+                this.setState({
+                    error: err.reason
+                })
+            }
+            return this.props.navigation.navigate('Home')
+        })
+
+    }
   render() {
+      const { name, phone, confirmPassword, password, error } = this.state
     return (
       <Container>
         <Content>
           <Form>
             <Item floatingLabel>
               <Label>Full Name</Label>
-              <Input />
+              <Input 
+                value={name}
+                onChangeText={text => this.setState({ name: text })}
+              />
             </Item>
             <Item floatingLabel>
               <Label>Phone Number</Label>
-              <Input />
+              <Input 
+                keyboardType="phone-pad"
+                value={phone}
+                onChangeText={text => this.setState({ phone: text })}
+                />
             </Item>
             <Item floatingLabel>
               <Label>Password</Label>
-              <Input />
+              <Input 
+                value={password}
+                onChangeText={text => this.setState({ password: text })}
+              />
             </Item>
             <Item floatingLabel last>
               <Label>Confirm Password</Label>
-              <Input />
+              <Input 
+                value={confirmPassword}
+                onChangeText={text => this.setState({ confirmPassword: text })}
+              />
             </Item>
           </Form>
-          <Body>
-              <Button transparent>
-                  <Text>Register</Text>
-              </Button>    
+          <Body style={{marginTop: 25}}>
+            <Button onPress={this.handleRegister}>
+                <Text>Register</Text>
+            </Button>    
+            <Text style={{color: 'red'}}>
+                {error.length ? error : null}
+            </Text>
           </Body>
         </Content>
       </Container>
