3f7cf67baf8a4171901e05c371965934310236fb
Olivier JM <manolivier93@gmail.com>
2018-11-21 16:32:24 +0200

    added a logout button

--- components/accounts/Profile.js
+++ components/accounts/Profile.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
-import { Container, Content, Card, CardItem, Text, Body } from "native-base";
+import { Container, Content, Card, CardItem, Text, Body, Button } from "native-base";
+import Meteor from 'react-native-meteor'
 
 export default class Profile extends Component {
+    logUserOut = () => {
+        Meteor.logout()
+        return this.props.navigation.navigate('Login')
+    }
   render() {
+      const user = Meteor.user()
+      if(!Meteor.userId()){
+          return (
+            <Container>
+            <Content padder>
+              <Card transparent>
+                <CardItem>
+                  <Body>
+                    <Text>
+                      Logged out
+                    </Text>
+                  </Body>
+                  <Body>
+                      <Button>
+                          <Text>
+                              Logout
+                          </Text>
+                      </Button>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>
+          )
+      }
     return (
       <Container>
         <Content padder>
@@ -10,8 +40,15 @@ export default class Profile extends Component {
             <CardItem>
               <Body>
                 <Text>
-                  This is just a Profile card with some text to boot.
+                  {user.profile.name}
                 </Text>
+              </Body>
+              <Body>
+                  <Button onPress={this.logUserOut}>
+                      <Text>
+                          Logout
+                      </Text>
+                  </Button>
               </Body>
             </CardItem>
           </Card>
