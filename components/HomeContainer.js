1f7a6f2b915b89226b99192b36eb1f11c58bf649
Olivier JM <manolivier93@gmail.com>
2018-12-07 07:51:39 +0200

    cleaned the home component

--- components/HomeContainer.js
+++ components/HomeContainer.js
@@ -59,48 +59,6 @@ class HomeScreen extends Component{
                 <List
                   dataArray={posts}
                   renderRow={post => (
-                    // <Card>
-                    //   <CardItem>
-                    //     <Right>
-                    //       <Body>
-                    //         <Text>{post.meta.title}</Text>
-                    //       </Body>
-                    //     </Right>
-                    //   </CardItem>
-                    //   <CardItem cardBody>
-                    //     <Image source={{uri: `http://10.1.0.149:3000/cdn/storage/images/${post._id}/original/${post._id}.${post.ext}`}}
-                    //        style={{height: 200, width: null, flex: 1}}/>
-                    //   </CardItem>
-                    //   <CardItem>
-                    //     <Body>
-                    //       <Text>
-                    //         {post.meta.content}
-                    //       </Text>
-                    //     </Body>
-                    //   </CardItem>
-                    //   <CardItem>
-                    //     <Right>
-                    //       <Text>{post.meta.createdAt && post.meta.createdAt.toLocaleString()}</Text>
-                    //     </Right>
-                    //   </CardItem>
-                    // </Card>
-                  //   <ListItem avatar>
-                  //   <Left>
-                  //     <Thumbnail square source={{uri: `http://10.1.0.149:3000/cdn/storage/images/${post._id}/original/${post._id}.${post.ext}`}} />
-                  //   </Left>
-                  //   <Body>
-                  //   <Text>{post.meta.title}</Text>
-                  //     <Text note numberOfLines={1}>{post.meta.content}</Text>
-                  //   </Body>
-                  //   <Right>
-                  //     <Button transparent>
-                  //       <Text>View</Text>
-                  //     </Button>
-                  //   </Right>
-                  //   <Body>
-                  //   <Text>{post.meta.createdAt && post.meta.createdAt.toLocaleString()}</Text>
-                  //   </Body>
-                  // </ListItem>
                   <ListItem avatar>
                       <Left>
                         <Thumbnail source={{ uri: `http://10.1.0.149:3000/cdn/storage/images/${post._id}/original/${post._id}.${post.ext}` }} />
@@ -131,10 +89,3 @@ class HomeScreen extends Component{
   }
 }
 export default HomeScreen
-// export default withTracker(params => {
-//   const handle = Meteor.subscribe("posts");
-//   return {
-//     postsReady: handle.ready(),
-//     posts: Meteor.collection("posts").find()
-//   };
-// })(HomeScreen);
