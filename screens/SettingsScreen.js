50894d34b11de8afbe91a921e2b07f8cabb8a076
Olivier JM <manolivier93@gmail.com>
2018-12-07 08:50:13 +0200

    changed the departments components

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
-export default class SettingsScreen extends React.Component {
+export default class Departments extends React.Component {
   static navigationOptions = {
-    title: 'Settings',
+    title: 'Departments',
     headerStyle: {
       backgroundColor: '#428cf4',
     },
@@ -12,9 +12,26 @@ export default class SettingsScreen extends React.Component {
 
   render() {
     return (
-      <Text>
-        Settings could be here
-      </Text>
+      <View style={styles.container}>
+        <Text style={styles.content}>
+          Coming soon ...
+        </Text>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  content: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+
+})
