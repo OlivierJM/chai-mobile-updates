16c4b7e9ad86af036fbc92b16631389aedb068b6
Olivier JM <manolivier93@gmail.com>
2018-12-07 08:19:40 +0200

    added route to details page

--- components/HomeContainer.js
+++ components/HomeContainer.js
@@ -62,7 +62,7 @@ class HomeScreen extends Component{
                 <List
                   dataArray={posts}
                   renderRow={post => (
-                  <ListItem avatar>
+                  <ListItem avatar onPress={() => this.props.navigation.navigate('Details')}>
                       <Left>
                         <Thumbnail source={{ uri: `http://10.1.0.149:3000/cdn/storage/images/${post._id}/original/${post._id}.${post.ext}` }} />
                       </Left>
